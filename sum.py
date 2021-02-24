3012d6ce989c2db0b5aab97af03a03540bd0c443
Tiago Silva <tiago.pinhal96@gmail.com>
2021-02-24 17:06:10 +0000

    Add feature that stores inputA, inputB and operation with result, the last one as a string

--- sum.py
+++ sum.py
@@ -2,8 +2,28 @@ def somar_dois(a, b):
     return f"{a}+{b}={a+b}"
 
 
-a = int(input("Numero 1: "))
-b = int(input("Numero 2:"))
+inputA = []
+inputB = []
+results = []
 
-if __name__ == "__main__":
-    print(somar_dois(a, b))
+
+while True:
+
+    try:
+        a = int(input("Numero 1: "))
+        b = int(input("Numero 2: "))
+    except ValueError as v_error:
+        print("Erro Inserção de Valor: ", v_error)
+        break
+    except TypeError as t_error:
+        print("Erro de Tipo de Input: ", t_error)
+
+    result = somar_dois(a, b)
+    inputA.append(a)
+    inputB.append(b)
+    results.append(result)
+
+
+print(inputA)
+print(inputB)
+print(results)
