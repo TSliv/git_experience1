ce4f8fb337af930c2ac23e0056b5ed5f1bea2d6e
Tiago Silva <tiago.pinhal96@gmail.com>
2021-02-24 16:53:49 +0000

    Add feature to user input values

--- sum.py
+++ sum.py
@@ -2,4 +2,8 @@ def somar_dois(a, b):
     return f"{a}+{b}={a+b}"
 
 
-print(somar_dois(7, 10))
+a = int(input("Numero 1: "))
+b = int(input("Numero 2:"))
+
+if __name__ == "__main__":
+    print(somar_dois(a, b))
